619f5bc391519bbd3d75492a7b223ea7c1c8e194
Tamil Selvan <tamil.selvan.75457@gmail.com>
2026-08-30 13:28:25 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public int maxSubArray(int[] nums) 
+    {
+        int sum=nums[0];
+        int maxsum=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            sum=Math.max(nums[i],sum+nums[i]);
+            maxsum=Math.max(sum,maxsum);
+        }
+        return maxsum;
+    }
+}
